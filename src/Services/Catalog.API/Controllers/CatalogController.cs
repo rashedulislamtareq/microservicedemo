106f126222450b40e5f360cf1869f447dae067de
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2023-11-03 23:25:15 +0600

    Added API For Get Products

---
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Interfaces.Manager;
+using Catalog.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductManager _productManager;
+
+        public CatalogController(IProductManager productManager)
+        {
+            _productManager = productManager;   
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public IActionResult GetProducts() 
+        {
+            var products = _productManager.GetAll();
+            return Ok(products);
+        }
+    }
+}
--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using Catalog.API.HostingService;
+using Catalog.API.Interfaces.Manager;
+using Catalog.API.Manager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductManager, ProductManager>();
 
 //Add Hosted Service
 builder.Services.AddHostedService<AppHostedService>();
