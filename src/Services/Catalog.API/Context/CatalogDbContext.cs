bfb5736e608d84251deee31dd2656d7b8773eb14
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2023-11-03 22:30:58 +0600

    Added Manager Repository Add Seed With Hosted Service

---
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+        static string connectionString = config.GetConnectionString("Catalog.API");
+        static string databaseName = config.GetValue<string>("DatabaseName");
+
+        public CatalogDbContext() : base(connectionString, databaseName)
+        {
+        }
+    }
+}
--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using Catalog.API.HostingService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add Hosted Service
+builder.Services.AddHostedService<AppHostedService>();
 
 var app = builder.Build();
 
