f606976d52e281d302bda6ca108f2458bddaf8e8
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2023-12-08 17:12:36 +0600

    Added Discount API Project With Packages

---
+++ src/Services/Discount.API/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using Dapper;
+global using Discount.API.Models;
+global using Npgsql;
---
+++ src/Services/Discount.API/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.API.Repository;
+
+public interface ICouponRepository
+{
+    Task<Coupon> GetDiscount(string productId);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productId);
+}
