ebd777ff022fbab7ae918f4d30f5f4a094b6bf16
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2023-12-08 23:51:39 +0600

    Initially Added Discount Grpc Application

---
+++ src/Services/Discount.Grpc/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Dapper;
+global using Discount.Grpc.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Npgsql;
+global using Discount.Grpc.Repository;
---
+++ src/Services/Discount.Grpc/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.Grpc.Repository;
+
+public interface ICouponRepository
+{
+    Task<Coupon> GetDiscount(string productId);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productId);
+}
